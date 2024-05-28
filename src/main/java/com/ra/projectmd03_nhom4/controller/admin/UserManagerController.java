3d5185f7aa46bf62c25dae707ae2711524bda2a3
Nguyen Van Tien <nguyenvantien2910@gmail.com>
2024-05-28 19:52:24 +0700

    change file name

--- src/main/java/com/ra/projectmd03_nhom4/controller/admin/UserManagerController.java
+++ src/main/java/com/ra/projectmd03_nhom4/controller/admin/UserManagerController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/admin/user")
-public class UserController {
+public class UserManagerController {
     @Autowired
     private IUserService userService;
 
